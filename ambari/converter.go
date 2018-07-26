2b50f29d5aefd33b5637f4fc467bb829b7f375b3
Oliver Szabo <oleewere@gmail.com>
2018-07-26 18:33:48 +0200

    refactor

--- ambari/converter.go
+++ ambari/converter.go
@@ -5,27 +5,32 @@ func (a AmbariItems) ConvertResponse() Response {
 	response := Response{}
 	hosts := []Host{}
 	for _, item := range a.Items {
-		if hosts_val, ok := item["Hosts"]; ok {
-			host := Host{}
-			hostI := hosts_val.(map[string]interface{})
-			if hostname, ok := hostI["host_name"]; ok {
-				host.HostName = hostname.(string)
-			}
-			if ip, ok := hostI["ip"]; ok {
-				host.IP = ip.(string)
-			}
-			if public_host_name, ok := hostI["public_host_name"]; ok {
-				host.PublicHostname = public_host_name.(string)
-			}
-			if host_state, ok := hostI["host_state"]; ok {
-				host.HostState = host_state.(string)
-			}
-
-			hosts = append(hosts, host)
-		}
+		hosts = createHostsType(item, hosts)
 	}
 	if len(hosts) > 0 {
 		response.Hosts = hosts
 	}
 	return response
+}
+
+func createHostsType(item Item, hosts []Host) []Host {
+	if hosts_val, ok := item["Hosts"]; ok {
+		host := Host{}
+		hostI := hosts_val.(map[string]interface{})
+		if hostname, ok := hostI["host_name"]; ok {
+			host.HostName = hostname.(string)
+		}
+		if ip, ok := hostI["ip"]; ok {
+			host.IP = ip.(string)
+		}
+		if public_host_name, ok := hostI["public_host_name"]; ok {
+			host.PublicHostname = public_host_name.(string)
+		}
+		if host_state, ok := hostI["host_state"]; ok {
+			host.HostState = host_state.(string)
+		}
+
+		hosts = append(hosts, host)
+	}
+	return hosts
 }
