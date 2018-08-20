2a87d98e952aa93db159cd7b06a4fbb79f418164
Oliver Szabo <oleewere@gmail.com>
2018-08-20 19:19:50 +0200

    Add local command

---
+++ ambari/local.go
@@ -0,0 +1,39 @@
+// Copyright 2018 Oliver Szabo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ambari
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// RunLocalCommand run local system command
+func RunLocalCommand(command string, arg ...string) (error, string, string) {
+	outStr, errStr := "", ""
+	cmd := exec.Command(command, arg...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	outStr, errStr = string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+	return nil, outStr, errStr
+}
