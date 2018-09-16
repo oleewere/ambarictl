edb2f0e4abb09034499d473ac829a07805c6d48b
Oliver Szabo <oleewere@gmail.com>
2018-09-16 21:28:05 +0200

    Run playbooks with filters & Add print config example

--- ambari/playbook.go
+++ ambari/playbook.go
@@ -30,13 +30,19 @@ type Playbook struct {
 
 // Task represent a task that can be executed on an ambari hosts
 type Task struct {
-	Name          string `yaml:"name"`
-	Command       string `yaml:"command"`
-	Download      string `yaml:"download"`
-	Copy          string `yaml:"copy"`
-	LocalCopy     string `yaml:"local_copy"`
-	LocalCommand  string `yaml:"local_command"`
-	AmbariCommand string `yaml:"ambari_command"`
+	Name                string `yaml:"name"`
+	Command             string `yaml:"command"`
+	Download            string `yaml:"download"`
+	Copy                string `yaml:"copy"`
+	LocalCopy           string `yaml:"local_copy"`
+	LocalCommand        string `yaml:"local_command"`
+	AmbariCommand       string `yaml:"ambari_command"`
+	HostComponentFilter string `yaml:"host_component_filter"`
+	AmbariServerFilter  bool   `yaml:"ambari_server"`
+	AmbariAgentFilter   bool   `yaml:"ambari_agent"`
+	HostFilter          string `yaml:"hosts"`
+	ServiceFilter       string `yaml:"services"`
+	ComponentFilter     string `yaml:"components"`
 }
 
 // LoadPlaybookFile read a playbook yaml file and transform it to a Playbook object
@@ -52,7 +58,7 @@ func LoadPlaybookFile(location string) Playbook {
 		fmt.Print(err)
 		os.Exit(1)
 	}
-	fmt.Println(playbook)
+	fmt.Println(fmt.Sprintf("[Executing playbook: %v, file: %v]", playbook.Name, location))
 	return playbook
 }
 
@@ -61,7 +67,12 @@ func (a AmbariRegistry) ExecutePlaybook(playbook Playbook) {
 	tasks := playbook.Tasks
 	for _, task := range tasks {
 		if len(task.Command) > 0 {
-			a.RunRemoteHostCommand(task.Command, nil)
+			if task.AmbariAgentFilter {
+				a.RunRemoteHostCommand(task.Command, nil)
+			} else {
+				filter := CreateFilter(task.ServiceFilter, task.ComponentFilter, task.HostFilter, task.AmbariServerFilter)
+				a.RunRemoteHostCommand(task.Command, a.GetFilteredHosts(filter))
+			}
 		}
 	}
 }
